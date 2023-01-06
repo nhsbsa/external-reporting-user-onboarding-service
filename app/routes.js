e05425f672fb452488efc3c6f383cbd4dc02a808
Dean Cobb <deaco@DEACOs-MacBook-Pro.local>
2023-01-06 11:40:51 +0000

    Start of routes file

--- app/routes.js
+++ app/routes.js
@@ -5,15 +5,27 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/subjectArea/', function (req, res) {
+
+  // points to the name attribute name="incomesingle"
+  const incomeSingle = req.session.data['incomesingle']
+
+  // checking the value of the variable (incomeSingle) and directing to pages based on that value
+  if (incomeSingle === 'gp-practice') {
+    res.redirect('PAGE-1')
+  } else if (incomeSingle === 'primary-care-network') {
+    res.redirect('PAGE-2')
+  } else if (incomeSingle === 'trust-other'){
+    res.redirect('your-details')
+  } else {
+    res.redirect('index')
+  }
+
+})
+
+
+
 module.exports = router;
 
-router.get(/subjectArea/, function (req, res) {
-    if (req.query.incomesingle === "eden") {
-      res.redirect('page-example'); 
-    } else if (req.query.incomesingle === "eops") {
-      res.redirect('page-example');
-    } else if (req.query.incomesingle === "epact") {
-        res.redirect('page-example');
-      return;
-    }
-  });
+
+
