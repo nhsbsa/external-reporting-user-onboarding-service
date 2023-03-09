9c92aaeae315b560dc63ed288e9205458b69e8ce
jackcockerill <jack.cockerill@nhs.net>
2023-03-09 13:08:30 +0000

    additional views added to match v1.0 POC/MVP

--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,60 @@ router.post('/subjectArea/', function (req, res) {
 
 })
 
+router.post('/accessTypeEden/', function (req, res) {
 
+  // points to the name attribute name="incomesingle"
+  const incomeSingle = req.session.data['incomesingle']
+
+  // checking the value of the variable (incomeSingle) and directing to pages based on that value
+  if (incomeSingle === 'int-care') {
+    res.redirect('ics-details-eden')
+  } else if (incomeSingle === 'local-health') {
+    res.redirect('lhb-details-eden')
+  } else if (incomeSingle === 'prac-man'){
+    res.redirect('pm-details-eden')
+  } else if (incomeSingle === 'provider'){
+    res.redirect('provider-details-eden')
+  } else {
+    res.redirect('access-type-eden')
+  }
+
+})
+
+router.post('/epactOrgtype/', function (req, res) {
+
+  // points to the name attribute name="incomesingle"
+  const incomeSingle = req.session.data['incomesingle']
+
+  // checking the value of the variable (incomeSingle) and directing to pages based on that value
+  if (incomeSingle === 'ahsn') {
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'ccg') {
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'csu'){
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'gpp'){
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'ht') {
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'ishp'){
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'la'){
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'nlo') {
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'pcm'){
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'pcn'){
+    res.redirect('epact2-organisation-details')
+  } else if (incomeSingle === 'stip'){
+    res.redirect('epact2-organisation-details')
+  
+  } else {
+    res.redirect('organisation-type-epact2')
+  }
+
+})
 
 module.exports = router;
 
